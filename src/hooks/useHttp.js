c8a3ab9145ad05c65b6d8ca102304f7201993afe
Tony Kowalewski <tony@kowalewski.dev>
2024-03-25 17:52:07 +0100

    add custom http hook

---
+++ src/hooks/useHttp.js
@@ -0,0 +1,32 @@
+import { useState, useEffect, useCallback } from 'react';
+import sendHttpRequest from '../util/http';
+
+const useHttp = (url, confObj = {}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(
+    async function () {
+      setIsLoading(true);
+      try {
+        const respData = await sendHttpRequest(url, confObj);
+        setData(respData);
+      } catch (err) {
+        setError(err.message || 'Something went wrong');
+      }
+      setIsLoading(false);
+    },
+    [url, confObj]
+  );
+
+  useEffect(() => {
+    if (confObj.method !== 'POST') {
+      sendRequest();
+    }
+  }, [confObj, sendRequest]);
+
+  return [isLoading, data, error, sendRequest];
+};
+
+export default useHttp;
--- src/util/http.js
+++ src/util/http.js
@@ -1,31 +1,45 @@
-// GET available meals
-export async function fetchAvailableMeals() {
-  const res = await fetch('http://localhost:3000/meals');
+// // GET available meals
+// export async function fetchAvailableMeals() {
+//   const res = await fetch('http://localhost:3000/meals');
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch meals');
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch meals');
+//   }
+
+//   const data = await res.json();
+//   return data;
+// }
+
+// // POST - send complete order
+// export async function sendOrder(order) {
+//   // second argument for fetch is configuration object
+//   // headers - additional meta-data attached to the request
+//   const res = await fetch('http://localhost:3000/orders', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ order: order }),
+//   });
+
+//   if (!res.ok) {
+//     throw new Error("Failed to send user's order");
+//   }
+
+//   const data = await res.json();
+//   return data.message;
+// }
+
+// Send Generic HTTP request
+export default async function sendHttpRequest(url, confObj) {
+  const resp = await fetch(url, confObj);
+  const respData = await resp.json();
+
+  if (!resp.ok) {
+    throw new Error(
+      respData.message || 'Something went wrong. Failed to send request'
+    );
   }
 
-  const data = await res.json();
-  return data;
-}
-
-// POST - send complete order
-export async function sendOrder(order) {
-  // second argument for fetch is configuration object
-  // headers - additional meta-data attached to the request
-  const res = await fetch('http://localhost:3000/orders', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ order: order }),
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to send user's order");
-  }
-
-  const data = await res.json();
-  return data.message;
+  return respData;
 }
