ecdd039e0d2d5c17919319ba1084a94ffcadd208
Tony Kowalewski <tony@kowalewski.dev>
2024-03-25 14:59:29 +0100

    add POST logic for orders

--- src/components/Cart/CheckoutForm.jsx
+++ src/components/Cart/CheckoutForm.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { formatPrice } from '../../util/formatting';
+import { sendOrder } from '../../util/http';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
 import OrderStepsContext from '../../store/orderSteps-context';
@@ -7,19 +8,33 @@ import Input from '../UI/Input';
 import Button from '../UI/Button';
 
 const CheckoutForm = () => {
-  const { totalAmount } = useContext(CartContext);
+  const { items, totalAmount } = useContext(CartContext);
   const { progress, hideCheckout } = useContext(OrderStepsContext);
 
-  const submitHandler = (e) => {
+  // Submit Form Handler
+  const submitHandler = async (e) => {
     e.preventDefault();
     console.log('Submitted!');
 
     // get data from form inputs
     const fd = new FormData(e.target);
     const data = Object.fromEntries(fd.entries());
-    console.log(data);
+    // console.log(data);
+
+    // combine data with order info (cart)
+    const order = {
+      items,
+      totalAmount,
+      customer: data,
+    };
 
     // send data via http request to backend
+    try {
+      const resp = await sendOrder(order);
+      console.log(resp);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -29,12 +44,12 @@ const CheckoutForm = () => {
         <p>Total amount: {formatPrice(totalAmount)}</p>
 
         {/* Form Input */}
-        <Input id='fullName' label='Full Name' type='text' />
+        <Input id='name' label='Full Name' type='text' />
         <Input id='email' label='Your Email' type='email' />
         <Input id='street' label='Street' type='text' />
 
         <div className='control-row'>
-          <Input id='postalCode' label='Postal Code' type='number' />
+          <Input id='postal-code' label='Postal Code' type='number' />
           <Input id='city' label='City' type='text' />
         </div>
 
--- src/util/http.js
+++ src/util/http.js
@@ -9,3 +9,23 @@ export async function fetchAvailableMeals() {
   const data = await res.json();
   return data;
 }
+
+// POST - send complete order
+export async function sendOrder(order) {
+  // second argument for fetch is configuration object
+  // headers - additional meta-data attached to the request
+  const res = await fetch('http://localhost:3000/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ order: order }),
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to send user's order");
+  }
+
+  const data = await res.json();
+  return data.message;
+}
